7e1acb2fdad9ca3ce5861ccb7f16b7fd290809d2
Udhay <udhaykumar@yugasys.com>
2017-01-31 16:11:40 +0530

    Api second commit

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require("express");
 var app = express();
 var PORT = process.env.PORT || 3000;
+var db = [{id:1,name:"Udhay",msg:"Success"},{id:2,name:"sabarish",msg:"hi"}];
 
 app.get("/", function(req,res){
    
-    res.send(JSON.stringify({Name:"Udhay",Msg:"Success"}));
+    res.setHeader('content-type', 'application/json');
+    
+    res.send(JSON.stringify(db[0]));
     
 });
 
