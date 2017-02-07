e19da8872f05110cfc874265ba4fcb0783190368
Udhay <udhaykumar@yugasys.com>
2017-02-07 16:13:15 +0530

    search add

--- server.js
+++ server.js
@@ -29,15 +29,39 @@ app.get("/:id", function(req,res){
    
     else{
     
-        res.status(404).send(JSON.stringify("Nothing Found!"));
+        res.status(404).send(JSON.stringify("Not Found!"));
         
     }
   
 });
 
+app.get("/db/search", function(req,res){
+   
+    res.setHeader('content-type', 'application/json');
+    
+    var queryParams = req.query;
+   
+    var dat=db;
+    
+    if(queryParams.hasOwnProperty('id')){
+            
+    dat = _.findWhere(db,{id:parseInt(queryParams.id)});
+          
+    }
+    
+    if(dat)
+    
+    res.send(JSON.stringify(dat));
+   
+    res.status(404).send(JSON.stringify("Not Found!"));
+    
+});
+
 app.post("/datum",function(req,res){
     
     var body=_.pick(req.body,'id','name','message');
+    
+    res.setHeader('content-type', 'application/json');
     
     if(!_.isString(body.name.trim()) || !_.isNumber(body.id) || body.message.trim().length === 0){
         
@@ -53,6 +77,59 @@ app.post("/datum",function(req,res){
     
     res.json(body);
         
+});
+
+app.delete("/:id",function(req,res){
+    
+    res.setHeader('content-type', 'application/json');
+    
+    var dat = _.findWhere(db,{id:parseInt(req.params.id)});
+    
+    if(!dat){
+        
+       return res.status(404).send(JSON.stringify("Not Found!"));
+        
+    }
+    
+    db = _.without(db,dat);
+    
+    res.json(db);
+    
+});
+
+app.put('/:id',function(req,res){
+    
+    var body=_.pick(req.body,'id','name');
+              
+    var validAttributes={};
+    
+    if(body.hasOwnProperty('name') && _.isString(body.name)){
+        
+        var dat = _.findWhere(db,{id:parseInt(req.params.id)});
+        
+        if(dat){
+            
+            validAttributes.name=body.name;
+            
+            _.extend(dat,validAttributes );
+            
+            res.status(200).json(db);
+            
+        }
+        
+        else{
+            
+            res.status(404).json("Not Found");
+        }
+   
+    }
+    
+    else{
+        
+       res.status(198).json("Invalid Data");
+        
+    }
+debugger;
 });
 
 app.listen(PORT,function(){
